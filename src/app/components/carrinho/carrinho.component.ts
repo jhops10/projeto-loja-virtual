f9bdf93b8073f59f90e1813e14368443406c9d7f
João Paulo <joaowais@gmail.com>
2024-04-23 18:44:17 -0300

    Criando metodos no component Carrinho.ts

--- src/app/components/carrinho/carrinho.component.ts
+++ src/app/components/carrinho/carrinho.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduto } from '../../interfaces/Produto.interface';
+import { CarrinhoService } from '../../services/carrinho.service';
+import { TransacaoService } from '../../services/transacao.service';
+import { Transacao } from '../../models/Transacao';
 
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
-  styleUrl: './carrinho.component.css'
+  styleUrl: './carrinho.component.css',
 })
-export class CarrinhoComponent {
+export class CarrinhoComponent implements OnInit {
+  produtosSelecionados: IProduto[] = [];
+  compraFinalizada: boolean = false;
 
+  constructor(
+    private carrinhoService: CarrinhoService,
+    private transacaoService: TransacaoService
+  ) {}
+
+  ngOnInit(): void {
+    this.produtosSelecionados = this.carrinhoService.getProdutos();
+  }
+
+  calcularTotal(): number {
+    return this.carrinhoService.getTotal();
+  }
+
+  finalizarCompra(): void {
+    let total = this.calcularTotal();
+    let data = new Date();
+    let transacao = new Transacao(this.produtosSelecionados, total, data);
+    this.transacaoService.adicionarTransacao(transacao);
+    this.compraFinalizada = true;
+  }
 }
