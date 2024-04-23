7cb7e8d72e77a83ac59fc6fa9fc7400ad7334cf7
João Paulo <joaowais@gmail.com>
2024-04-23 19:40:01 -0300

    Projeto Finalizado

--- src/app/components/compras/compras.component.ts
+++ src/app/components/compras/compras.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduto } from '../../interfaces/Produto.interface';
+import { ITransacao } from '../../interfaces/Transacao.interface';
+import { CarrinhoService } from '../../services/carrinho.service';
+import { TransacaoService } from '../../services/transacao.service';
 
 @Component({
   selector: 'app-compras',
   templateUrl: './compras.component.html',
-  styleUrl: './compras.component.css'
+  styleUrl: './compras.component.css',
 })
-export class ComprasComponent {
+export class ComprasComponent implements OnInit {
+  produtosDoCarrinho: IProduto[] = [];
+  transacoes: ITransacao[] = [];
 
+  constructor(
+    private carrinhoService: CarrinhoService,
+    private transacaoService: TransacaoService
+  ) {}
+
+  ngOnInit(): void {
+    this.produtosDoCarrinho = this.carrinhoService.getProdutos();
+    this.transacoes = this.transacaoService.getTransacoes();
+  }
+
+  getTransacoes(): ITransacao[] {
+    return this.transacoes;
+  }
 }
